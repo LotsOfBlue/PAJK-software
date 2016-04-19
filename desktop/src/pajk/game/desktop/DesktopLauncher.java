270d12b8a592d31163bbc8b6ebe66aab2966d9f9
JonatanGust <jongust@student.chalmers.se>
2016-04-19 15:51:48 +0200

    gitgudp2

---
+++ desktop/src/pajk/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package pajk.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import pajk.game.PajkGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PajkGdxGame(), config);
+	}
+}
