9fa42d9f99e1105864d0d6dae30b4f84fa72fa97
JonatanGust <jongust@student.chalmers.se>
2016-04-20 15:31:03 +0200

    update of javadoc on unit & weapon

--- core/src/pajk/game/main/java/model/Unit.java
+++ core/src/pajk/game/main/java/model/Unit.java
@@ -4,8 +4,10 @@ import java.util.ArrayList;
 
 /**
  * Created by palm on 2016-04-15.
+ * The unit class is the representation of a carachter on the gameboard, it has stats & stuff
  */
 class Unit {
+    //Stats of a unit
     private int level = 1;
     private int experience = 1;
     private int strenght = 1;
@@ -19,9 +21,13 @@ class Unit {
     private int aid = 1;
     private UnitState state = UnitState.isReady;
     private UnitMovementType movementType = UnitMovementType.walking;
-    //private UnitAffinity affinity = UnitAfinity.;
-    private Weapon weapon ;
+    private Weapon weapon = new Weapon(Weapon.Type.Pike, );
     private ArrayList<Item> itemList;
+    private Allegiance allegiance;
+    public enum Allegiance{
+        human,
+        ai
+    }
     public enum UnitState{
         isReady,
         isMoved,
@@ -32,7 +38,8 @@ class Unit {
         riding,
         flying
     }
-    Unit(/*int level,
+    Unit(Allegiance allegiance
+                    /*int level,
                     int experience,
                     int strenght,
                     int skill,
@@ -46,27 +53,112 @@ class Unit {
                     ConditionType condition,
                     MovementType movementType,
                     AffinityType affinity*/){
-
+        this.allegiance = allegiance;
 
     }
-    //Getters
+    //----------------------------------------------Getters
+
+    /**
+     * @return the Level of the unit
+     */
     public int getLevel(){ return level;}
+
+    /**
+     * @return the Experience of the unit
+     */
     public int getExperience(){ return experience;}
+
+    /**
+     * @return the Strenght of the unit
+     */
     public int getStrenght(){ return strenght;}
+
+    /**
+     * @return the Skill of the unit
+     */
     public int getSkill(){return skill;}
+
+    /**
+     * @return the Speed of the unit
+     */
     public int getSpeed(){return speed;}
+
+    /**
+     * @return the Luck of the unit
+     */
     public int getLuck(){return luck;}
+
+    /**
+     * @return the Defence of the unit
+     */
     public int getDefence(){return defence;}
+
+    /**
+     * @return the Resistance of the unit
+     */
     public int getResistance(){return resistance;}
+
+    /**
+     * @return the Movement of the unit
+     */
     public int getMovement(){return movement;}
+
+    /**
+     * @return the Constitution of the unit
+     */
     public int getConstitution(){return constitution;}
+
+    /**
+     * @return the Aid of the unit
+     */
     public int getAid(){return aid;}
-    public UnitState getUnitState(){return state;}
+
+    /**
+     * @return the MovementType of the unit
+     */
     public UnitMovementType getMovementType(){return movementType;}
     //private UnitAffinity affinity = UnitAfinity.;
-    public Weapon getWeapon(){return weapon;}
+
+
+
+    /**
+     * @return all the Items of the unit
+     */
     public ArrayList<Item> getItemList(){return itemList;}
 
+    /**
+     * @return the Weapon Type of the unit
+     */
+    public Weapon.Type getWeaponType(){return getWeaponType();}
+
+    /**
+     * @return the weapon damage of the unit
+     */
+    public int getWeaponDamage() {
+        return weapon.getDamage();
+    }
+
+    /**
+     * @return the attack Range of the unit
+     */
+    public int getWeaponRange(){return weapon.getRange();}
+
+    /**
+     * @return the current State of the unit, see @UnitState
+     */
+    public UnitState getState() {return state;}
+
+    /**
+     * @return the Allegiance of the unit, ethier human or ai
+     */
+    public Allegiance getAllegiance() {return allegiance;}
+    //--------------------------------------------------
+
+    /**
+     * Increases the experience of the unit by the @param value
+     * @param experience
+     */
     public void addExperience(int experience){this.experience+=experience;}
+
 
 }
--- core/src/pajk/game/main/java/model/Weapon.java
+++ core/src/pajk/game/main/java/model/Weapon.java
@@ -2,17 +2,12 @@ package pajk.game.main.java.model;
 
 /**
  * Created by jonatan on 20/04/2016.
+ * Weapons are special items of a special type, with a range, damage and critmultiplier value.
  */
 class Weapon extends Item{
-    private WeaponType weaponType;
+    private Type weaponType;
     private int range, damage, critMult;
-    Weapon(WeaponType weaponType, int range, int damage, int criticalMultiplier){
-        this.weaponType = weaponType;
-        this.critMult = critMult;
-        this.damage = damage;
-        this.range = range;
-    }
-    public enum WeaponType{
+    public enum Type{
         Sword,
         Axe,
         Pike,
@@ -20,4 +15,19 @@ class Weapon extends Item{
         Staff,
         Book
     }
+
+    Weapon(Type weaponType, int range, int damage, int criticalMultiplier){
+        this.weaponType = weaponType;
+        this.critMult = critMult;
+        this.damage = damage;
+        this.range = range;
+    }
+
+    public int getCritMult() {return critMult;}
+
+    public int getDamage() {return damage;}
+
+    public int getRange() {return range;}
+
+    public Type getWeaponType() {return weaponType;}
 }
