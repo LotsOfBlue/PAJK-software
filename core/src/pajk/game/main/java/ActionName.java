beb5de007c0d7206e770ae52c86d8e08ebdc6441
Knaprig- <gustav.grannsjo@gmail.com>
2016-04-22 13:40:22 +0200

    Added some files

---
+++ core/src/pajk/game/main/java/ActionName.java
@@ -0,0 +1,8 @@
+package pajk.game.main.java;
+
+/**
+ * Created by Gustav on 2016-04-22.
+ */
+public enum ActionName {
+        UP,DOWN,LEFT,RIGHT,ENTER,BACK;
+}
---
+++ core/src/pajk/game/main/java/model/MainState.java
@@ -0,0 +1,33 @@
+package pajk.game.main.java.model;
+
+import pajk.game.main.java.ActionName;
+
+/**
+ * Created by Gustav on 2016-04-22.
+ */
+public class MainState implements State {
+
+    Board board;
+
+    public MainState(Board board){
+        this.board = board;
+    }
+
+    @Override
+    public void performAction(ActionName action) {
+        switch (action){
+            case UP:
+                board.moveCursor(Board.Direction.NORTH);
+                break;
+            case LEFT:
+                board.moveCursor(Board.Direction.WEST);
+                break;
+            case RIGHT:
+                board.moveCursor(Board.Direction.EAST);
+                break;
+            case DOWN:
+                board.moveCursor(Board.Direction.SOUTH);
+                break;
+        }
+    }
+}
