133054d0ea5d166a26beb7952e14937f7e1923f2
Knaprig- <gustav.grannsjo@gmail.com>
2016-04-22 13:47:02 +0200

    Tog bort PAlms testkod

--- core/src/pajk/game/PajkGdxGame.java
+++ core/src/pajk/game/PajkGdxGame.java
@@ -5,8 +5,6 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import pajk.game.main.java.States.GameStateManager;
-import pajk.game.main.java.States.MainMenuState;
 import pajk.game.main.java.controller.Controller;
 import pajk.game.main.java.model.StateManager;
 
@@ -16,7 +14,6 @@ public class PajkGdxGame extends ApplicationAdapter {
 
 	public static final String TITLE = "Pajkification";
 
-	private GameStateManager gameStateManager;
 	Controller gameController = new Controller(StateManager.getInstance());
 
 	private SpriteBatch batch;
@@ -26,16 +23,12 @@ public class PajkGdxGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 //		img = new Texture("badlogic.jpg");
-		gameStateManager = new GameStateManager();
-		gameStateManager.push(new MainMenuState(gameStateManager));
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		gameStateManager.update(Gdx.graphics.getDeltaTime());	//update time
-		gameStateManager.render(batch);
 		listenToKeys();
 //		batch.begin();
 //		batch.draw(img, 0, 0);
