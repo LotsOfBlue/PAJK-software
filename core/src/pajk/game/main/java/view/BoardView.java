ec56f64fe188a1bbfb6b54c8059c099a31c8a06b
Palmmro <palmro@student.chalmers.se>
2016-04-28 14:23:14 +0200

    draws move (and attack) overlay on tiles

--- core/src/pajk/game/main/java/view/BoardView.java
+++ core/src/pajk/game/main/java/view/BoardView.java
@@ -4,6 +4,7 @@ package pajk.game.main.java.view;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.Texture;
 import pajk.game.main.java.model.StateManager;
+import pajk.game.main.java.model.Tile;
 
 
 /**
@@ -16,12 +17,18 @@ public class BoardView extends GameView{
     private Texture img;
     private Texture unit;
     private Texture cursor;
+    private Texture overlayMove;
+    private Texture overlayAttack;
     private int tileWidth = 64;
+    private SpriteBatch spriteBatch;
 
     public BoardView(){
         img = new Texture("grass-tile");
         unit = new Texture("unit-sprite");
         cursor = new Texture("cursor");
+        overlayMove = new Texture("overlayBlue.png");
+        overlayAttack = new Texture("overlayRed.png");
+
         this.gameModel = StateManager.getInstance();
     }
 
@@ -32,40 +39,58 @@ public class BoardView extends GameView{
 
     @Override
     public void render(SpriteBatch spriteBatch) {
+        this.spriteBatch = spriteBatch;
         spriteBatch.begin();
-        drawBoard(spriteBatch);
-        drawCursor(spriteBatch);
+        drawBoard();
+        drawCursor();
         spriteBatch.end();
     }
 
-    private void drawUnit(SpriteBatch spriteBatch,int x, int y){
-        draw(spriteBatch,x,y,unit);
+    private void drawUnit(int x, int y){
+        draw(x,y,unit);
     }
 
-    private void drawTile(SpriteBatch spriteBatch, int x, int y){
-        draw(spriteBatch,x,y,img);
+    private void drawTile(int x, int y){
+        draw(x,y,img);
+
     }
 
-    private void drawCursor(SpriteBatch spriteBatch){
+    private void drawCursor(){
         int cursorX = gameModel.getBoard().getCursorTile().getX();
         int cursorY = gameModel.getBoard().getCursorTile().getY();
-        draw(spriteBatch,cursorX,cursorY,cursor);
+        draw(cursorX,cursorY,cursor);
     }
 
-    private void draw(SpriteBatch spriteBatch, int x, int y, Texture texture){
+    /**
+     * Draws texture on board.
+     * Converts from "board coordinates" to "pixel coordinates"
+     * @param spriteBatch
+     * @param x
+     * @param y
+     * @param texture
+     */
+    private void draw(int x, int y, Texture texture){
         int pixelX = x*(tileWidth+1);
         int pixelY = (gameModel.getBoard().getBoardHeight() - 1 - y)*(tileWidth+1);
         spriteBatch.draw(texture,pixelX,pixelY);
     }
 
-    private void drawBoard(SpriteBatch spriteBatch){
+    /**
+     * Draws board with units and overlay
+     *
+     */
+    private void drawBoard(){
 
         for(int x = 0; x < gameModel.getBoard().getBoardWidth(); x++){
             for(int y = 0; y < gameModel.getBoard().getBoardHeight(); y++){
-                drawTile(spriteBatch,x,y);
-                if(gameModel.getBoard().getTile(x,y).hasUnit()){   //
-                    //gameModel.getBoard().getTile(y,x).hasUnit();
-                    drawUnit(spriteBatch,x,y);
+                drawTile(x,y);
+                if(gameModel.getBoard().getTile(x,y).hasUnit()){
+                    drawUnit(x,y);
+                }
+                if(gameModel.getBoard().getTile(x,y).getOverlay() == Tile.Overlay.MOVEMENT){
+                    draw(x,y, overlayMove);
+                } else if(gameModel.getBoard().getTile(x,y).getOverlay() == Tile.Overlay.MOVEMENT){
+                    draw(x,y, overlayAttack);
                 }
 
             }
