86048c8eea0ad031174db03f17324bebffb54e38
Palmmro <palmro@student.chalmers.se>
2016-05-15 16:13:56 +0200

    prints text on status screen

--- core/src/pajk/game/main/java/view/StatusView.java
+++ core/src/pajk/game/main/java/view/StatusView.java
@@ -8,6 +8,8 @@ import pajk.game.main.java.model.GameModel;
 import pajk.game.main.java.model.StatusState;
 import pajk.game.main.java.model.Unit;
 
+import java.util.ArrayList;
+
 /**
  * Created by palm on 2016-05-14.
  */
@@ -44,7 +46,58 @@ public class StatusView extends AbstractGameView{
 
         font.getData().setScale(2,2);
 
-        font.draw(spriteBatch, unit.getName(),x,y+camera.viewportHeight/2);
+
+        String name = unit.getName();
+        String health = "Health: "+unit.getHealth() + "/" + unit.getMaxHealth();
+        String level = "Level: " +unit.getLevel();
+        String defence = "Defence: " +unit.getDefence();
+        String weapon = "Weapon: " +unit.getWeapon().getWeaponType();
+        String exp = "Experience: " +unit.getExperience();
+        String strength = "Strength: " +unit.getStrength();
+        String might = "Might: " +unit.getMight();
+        String skill = "Skill: " +unit.getSpeed();
+        String luck = "Luck: " + unit.getLuck();
+        String resistance = "Resistance: " +unit.getResistance();
+
+        ArrayList<String> stats = new ArrayList<>();
+        stats.add(name);
+        stats.add(health);
+        stats.add(level);
+        stats.add(defence);
+        stats.add(weapon);
+        stats.add(exp);
+        stats.add(strength);
+        stats.add(might);
+        stats.add(skill);
+        stats.add(luck);
+        stats.add(resistance);
+
+
+        /*
+        private String name;
+    private int level = 1;
+    private int experience = 1;
+    private int health = 20;
+    private int maxHealth = 20;
+    private int strength = 5;
+    private int might = 5;
+    private int skill = 5;
+    private int speed = 5;
+    private int luck = 5;
+    private int defence = 5;
+    private int resistance = 5;
+    private int movement = 3;
+    private int constitution = 5;
+    private int aid = 5;
+         */
+
+        font.draw(spriteBatch,name,x,y+ camera.viewportHeight - 100);
+        for(int i = 1; i < stats.size(); i++){
+            font.draw(spriteBatch, stats.get(i),x,y + camera.viewportHeight -110 - 30*i);
+        }
+
+//        font.draw(spriteBatch, unit.getName(),x,y+camera.viewportHeight/2);
+
         spriteBatch.end();
     }
 
