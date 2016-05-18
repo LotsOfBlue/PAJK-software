f68662aa162469108753264992a58a6a61dbdf50
Palmmro <palmro@student.chalmers.se>
2016-05-18 13:24:45 +0200

    adds comments for MainView

--- core/src/pajk/game/main/java/view/MainView.java
+++ core/src/pajk/game/main/java/view/MainView.java
@@ -10,24 +10,19 @@ import pajk.game.main.java.model.GameModel;
  */
 public class MainView {
 
-    private BoardView boardView = null;
+    private BoardView boardView;
     private CombatView combatView;
     private UnitMenuView menuView;
     private StatusView statusView;
     private EndView endView;
     private GameModel gameModel = GameModel.getInstance();
     private OrthographicCamera camera;
-//    private OrthographicCamera camera;
 
+    /**
+     * Creates the MainView
+     * MainView creates all other views and draws them. The seperate View classes decides if it should be drawn or not.
+     */
     public MainView(){
-        /*float w = Gdx.graphics.getWidth();
-        float h = Gdx.graphics.getHeight();
-        camera = new OrthographicCamera(w, h);
-        boardView = new BoardView(camera);
-        combatView = new CombatView();
-        menuView = new UnitMenuView(camera);
-        statusView = new StatusView(camera);
-        endView = new EndView(camera);*/
         makeNewViews();
 
 
