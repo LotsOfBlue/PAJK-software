d2bdcc046f815fe4afea23d6a603798af3c1144b
JonatanGust <jongust@student.chalmers.se>
2016-05-25 15:17:12 +0200

    fixed my shit

--- core/src/pajk/game/main/java/view/CombatView.java
+++ core/src/pajk/game/main/java/view/CombatView.java
@@ -28,6 +28,9 @@ public class CombatView extends AbstractGameView {
     private Unit activeUnit;
     private Unit targetUnit;
 
+
+    private final int tileSide = ViewUtils.TILE_WIDTH;
+
     private int firstDamageFromActiveUnit = 0;
     private boolean firstHitFromActiveUnit = false;
     private boolean firstCritFromActiveUnit = false;
@@ -72,9 +75,8 @@ public class CombatView extends AbstractGameView {
     private Animation createAnimationFrom(String filePath){
         if(unitAnimationHashMap.isEmpty() || !unitAnimationHashMap.containsKey(filePath)){
             Texture tempTexture = new Texture(filePath);
-            int tileSize = ViewUtils.TILE_WIDTH;
             float animationDuration = animationTime/2f;
-            TextureRegion[][] tempTextureRegions = TextureRegion.split(tempTexture, tileSize, tileSize);
+            TextureRegion[][] tempTextureRegions = TextureRegion.split(tempTexture, tileSide, tileSide);
             TextureRegion[] tempTextureRegion = new TextureRegion[tempTextureRegions.length * tempTextureRegions[0].length];
             int index = 0;
             for (int i = 0; i < tempTextureRegions.length; i++) {
@@ -132,21 +134,21 @@ public class CombatView extends AbstractGameView {
 
         switch (combatDrawState) {
             case ACTIVE_FIRST_HIT:
-                drawCombat(activeUnit,
+                drawThisCombat(activeUnit,
                         targetUnit,
                         true,
                         CombatDrawState.ENEMY_HIT,
                         ActionName.COMBAT_ACTIVE_HIT);
                 break;
             case ENEMY_HIT:
-                drawCombat(targetUnit,
+                drawThisCombat(targetUnit,
                         activeUnit,
                         attackFromEnemyUnit,
                         CombatDrawState.ACTIVE_SECOND_HIT,
                         ActionName.COMBAT_TARGET_HIT);
                 break;
             case ACTIVE_SECOND_HIT:
-                drawCombat(activeUnit,
+                drawThisCombat(activeUnit,
                         targetUnit,
                         secondAttackFromActiveUnit,
                         CombatDrawState.ACTIVE_FIRST_HIT,
@@ -155,19 +157,18 @@ public class CombatView extends AbstractGameView {
         }
     }
 
-    private void drawCombat(Unit activeUnit, Unit targetUnit, Boolean doesAttack, CombatDrawState nextDrawState, ActionName nextModelAction){
+    private void drawThisCombat(Unit activeUnit, Unit targetUnit, Boolean doesAttack, CombatDrawState nextDrawState, ActionName nextModelAction){
         float frame = (TimeUtils.millis() - timeStamp) / 1000f;
-        //Are we done?
+        //Are we done? Else do animation, or stationary with number
         if(frame >= animationTime || !doesAttack){
             combatDrawState = nextDrawState;
             gameModel.performAction(nextModelAction);
-            timeStamp = TimeUtils.millis();
+            reset();
+            return;
         }else  if(frame < animationTime/2f){
             drawAttackAnimation(activeUnit, frame);
         }else {
             drawAttackAnimation(activeUnit, 0);
-        }
-        if(frame >= animationTime/2f){
             drawDamageNumber(activeUnit, frame);
         }
         drawAttackAnimation(targetUnit, 0);
@@ -196,20 +197,6 @@ public class CombatView extends AbstractGameView {
         }
 
         drawAttackFrame(unit, textureRegion);
-    }
-
-    /**
-     * Checks what terrain the tile has and draws it.
-     * @param tile the tile to be drawn.
-     */
-    private void drawTile(Tile tile){
-        Texture texture = ViewUtils.getTileTexture(tile);
-        float[] pos = calcDrawPos(tile);
-
-        TextureRegion txtReg = new TextureRegion(texture, 0, 0, ViewUtils.TILE_WIDTH, ViewUtils.TILE_WIDTH - hpBar.getHeight());
-
-        draw(txtReg, pos[0], pos[1]);
-        draw(gridTexture, pos[0], pos[1]);
     }
 
     private void drawDamageNumber(Unit unit, float frame){
@@ -242,29 +229,55 @@ public class CombatView extends AbstractGameView {
     }
 
     private void drawDamageMessage(Unit targetUnit, float frame, boolean doesAttack, boolean doesHit, boolean doesCrit, int damage){
-        float uPos[] = {0f,0f};
+        float uPos[];
         float scale = frame * 1.1f;
         font.getData().setScale(scale);
         font.setColor(Color.BLACK);
-        String message = "null";
+        String message;
 
-        if(!message.equals("null")){
-            draw(message, uPos[0]+ViewUtils.TILE_WIDTH/3, uPos[1]+ViewUtils.TILE_WIDTH * 1.2f);
+        if(doesAttack){
+            uPos = calcDrawPos(targetUnit);
+            if(doesHit){
+                if(doesCrit){
+                    draw("CRIT", uPos[0] + tileSide / 3f, uPos[1]+ (tileSide*1.6f));
+                }
+                message = ""+damage;
+            }else{
+                message = "MISS";
+            }
+            draw(message, uPos[0] + tileSide / 3f, uPos[1] + tileSide * 1.2f);
         }
+
+
     }
+    /**
+     * Checks what terrain the tile has and draws it.
+     * @param tile the tile to be drawn.
+     */
+    private void drawTile(Tile tile){
+        Texture texture = ViewUtils.getTileTexture(tile);
+        float[] pos = calcDrawPos(tile);
+
+        TextureRegion txtReg = new TextureRegion(texture, 0, 0, tileSide, tileSide - hpBar.getHeight());
+
+        draw(txtReg, pos[0], pos[1]);
+        draw(gridTexture, pos[0], pos[1]);
+    }
+
+
 
     private float[] calcDrawPos(Unit unit){
         float uPos[] = new float[2];
         Tile myTile = board.getPos(unit);
-        uPos[0] = (myTile.getX() ) * ViewUtils.TILE_WIDTH;
-        uPos[1] = (board.getBoardHeight() - myTile.getY() - 1) * ViewUtils.TILE_WIDTH;
+        uPos[0] = (myTile.getX() ) * tileSide;
+        uPos[1] = (board.getBoardHeight() - myTile.getY() - 1) * tileSide;
         return uPos;
     }
 
     private float[] calcDrawPos(Tile tile){
         float uPos[] = new float[2];
-        uPos[0] = (tile.getX() ) * ViewUtils.TILE_WIDTH;
-        uPos[1] = (board.getBoardHeight() - tile.getY() - 1) * ViewUtils.TILE_WIDTH;
+        uPos[0] = (tile.getX() ) * tileSide;
+        uPos[1] = (board.getBoardHeight() - tile.getY() - 1) * tileSide;
         return uPos;
     }
 
